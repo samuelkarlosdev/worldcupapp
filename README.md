8d52d68baa2217554501afd3c76178879c558de2
samuelkarlosdev <skdevelopment21@gmail.com>
2023-04-22 16:29:44 -0300

    Itens List Selected OnLongPress

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # world_cup_app
 
-Esse projeto tem como base aplicações de conhecimentos de Dart e Flutter.
+Esse projeto tem como base aplicações de conhecimentos de Dart e Flutter:
+- Lista de seleções carregada dinamicamente em um ListView baseado em um repositório estático;
+- Selecionando itens da lista com evento de onLongPress;
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:world_cup_app/models/country_model.dart';
 import 'package:world_cup_app/repositories/country_repository.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final countryList = CountryRepository.countryList;
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final countryList = CountryRepository.countryList;
+  List<Country> listCountrySelected = [];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Copa do Mundo'),
@@ -18,9 +25,27 @@ class HomePage extends StatelessWidget {
       body: ListView.separated(
           itemBuilder: (BuildContext context, int country) {
             return ListTile(
-              leading: Image.asset(countryList[country].flag),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              leading: (listCountrySelected.contains(countryList[country]))
+                  ? CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : Image.asset(countryList[country].flag),
               title: Text(countryList[country].name),
-              trailing: Text(countryList[country].titles.toString()),
+              trailing: Text(
+                countryList[country].titles.toString(),
+              ),
+              selected: listCountrySelected.contains(countryList[country]),
+              selectedTileColor: Colors.blue[50],
+              onLongPress: () {
+                setState(() {
+                  (listCountrySelected.contains(countryList[country]))
+                      ? listCountrySelected.remove(countryList[country])
+                      : listCountrySelected.add(countryList[country]);
+                });
+              },
             );
           },
           padding: const EdgeInsets.all(16),
