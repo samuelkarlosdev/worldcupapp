6b0b601668148646d8a05f1425859640916f3e31
samuelkarlosdev <skdevelopment21@gmail.com>
2023-05-03 18:39:52 -0300

    Refactoring Page County Details

--- lib/models/country_model.dart
+++ lib/models/country_model.dart
@@ -2,10 +2,18 @@ class Country {
   String name;
   String flag;
   int titles;
+  String titlesforyears;
+  String vicesyears;
+  String thirdplaceyears;
+  String fourthplaceyear;
 
   Country({
     required this.name,
     required this.flag,
     required this.titles,
+    required this.titlesforyears,
+    required this.vicesyears,
+    required this.thirdplaceyears,
+    required this.fourthplaceyear,
   });
 }
--- lib/pages/country_details_page.dart
+++ lib/pages/country_details_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:world_cup_app/models/country_model.dart';
 
 class CountryDetailsPage extends StatefulWidget {
@@ -18,6 +16,103 @@ class _CountryDetailsPageState extends State<CountryDetailsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.country.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Image.asset(widget.country.flag),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Títulos:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                Text(
+                  widget.country.titlesforyears,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Vices:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                Text(
+                  widget.country.vicesyears,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '3° Lugar:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                Text(
+                  widget.country.thirdplaceyears,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '4° Lugar:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                Text(
+                  widget.country.fourthplaceyear,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
